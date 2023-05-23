0f3426f86cda0383e9d24be036b141313250664a
noia1111 <132414908+noia1111@users.noreply.github.com>
2023-05-23 11:47:09 +0000

    [finish]3-18

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -8,26 +8,27 @@ class PostImagesController < ApplicationController
     @post_image.save
     redirect_to post_images_path
   end
-  
+
   def index
     @post_images = PostImage.all
   end
 
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
-  
+
   def destroy
     post_image = PostImage.find(params[:id])
     post_image.destroy
     redirect_to "/post_images"
   end
-  
+
     # 投稿データのストロングパラメータ
   private
 
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
-  
+
 end
