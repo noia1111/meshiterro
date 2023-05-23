44cf75801b8c0024b65c8d1e69b28f65631ecf78
noia1111 <132414908+noia1111@users.noreply.github.com>
2023-05-23 07:15:48 +0000

    [finish]3-17

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
   devise_for :users
   root to:"homes#top"
   get "homes/about" => "homes#about", as: "about"
